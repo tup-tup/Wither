7269b247dde5866a8144760bbc655fc50ac97903
tup-tup <60755294+tup-tup@users.noreply.github.com>
2020-02-08 00:06:19 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView : 2,
+    loop: true,
+    navigation: {
+        nextEl: '.arrow',
+    },
+});
+
+var menuButton = document.querySelector('.menu-button');
+var menu = document.querySelector('.header'); 
+    menuButton.addEventListener ('click', function() {
+    ValidityState.
+    menuButton.classList.toggle('menu vat-ockfdfs')
+    Benu.classList.toggle('header-active');
+});
